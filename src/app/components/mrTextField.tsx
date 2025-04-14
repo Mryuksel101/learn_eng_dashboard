2ac3477f7ca1a5572279b6348d5191b31e78cb50
Mustafa Yuksel <mryuksel@gmail.com>
2025-04-14 18:13:52 +0300

    Add placeholder prop to TextField component for enhanced usability

--- src/app/components/mrTextField.tsx
+++ src/app/components/mrTextField.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 interface TextFieldProps {
     label?: string;
+    placeholder?: string;
     type?: string;
     value?: string;
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const TextField: React.FC<TextFieldProps> = ({ label, type = 'text', value, onChange }) => {
+const TextField: React.FC<TextFieldProps> = ({ label, placeholder, type = 'text', value, onChange }) => {
     const [isFocused, setFocus] = React.useState<boolean>(false);
 
     const handleFocus = () => {
@@ -16,11 +17,10 @@ const TextField: React.FC<TextFieldProps> = ({ label, type = 'text', value, onCh
     const handleBlur = () => setFocus(false);
 
     return (
-        // h-12.8
-        <div className='relative h-12.8'>
+        <div className='relative overflow-visible'>
             <input
-                className={` top-0 w-full px-4 py-3 rounded-3xl border-2 border-gray-700 text-gray-200 placeholder-gray-400
-                shadow-sm transition-all duration-200 ease-in-out
+                className={`w-full px-4 py-3 rounded-3xl border border-gray-700 text-gray-200 placeholder-gray-400
+                shadow-sm transition-all duration-300 ease-in-out
                 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent
              ${isFocused ? 'bg-[171717]' : 'text-gray-200'}`}
                 type={type}
@@ -29,14 +29,16 @@ const TextField: React.FC<TextFieldProps> = ({ label, type = 'text', value, onCh
                 onFocus={handleFocus}
                 onBlur={handleBlur}
             />
-            <label style={
-                {
-                    backgroundColor: isFocused || value ? '#0a0a0a' : 'transparent',
+            <label
+                style={
+                    {
+                        backgroundColor: isFocused || value ? '#0a0a0a' : 'transparent',
+                    }
                 }
-            } className={`absolute left-4 transition-all duration-200 ease-in-out bg-[0a0a0a] pointer-events-none
+                className={`absolute left-4 transition-all duration-300 ease-in-out bg-[171717]
                 ${isFocused || value
-                    ? '-top-2.5 text-xs px-1 text-blue-500'
-                    : 'top-1/2 transform -translate-y-1/2 text-gray-400'}`}>
+                        ? '-top-2.5 text-xs px-1 text-blue-500 opacity-100 transform scale-100'
+                        : 'top-1/2 transform -translate-y-1/2 text-gray-400 opacity-90 scale-105'}`}>
                 {label}
             </label>
         </div>
